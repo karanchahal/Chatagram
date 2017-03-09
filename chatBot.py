b3315af1576fd1e0ffe59a1f899c7d5fb170bd11
Karan <karanbleep@gmail.com>
2017-03-10 05:23:43 +0530

    Completed auth/veriffication and added balance, acc statement etc

--- chatBot.py
+++ chatBot.py
@@ -2,51 +2,71 @@ from watson_developer_cloud import ConversationV1
 import json
 import fileinput
 
+#Opening Files
 with open('config.json') as data_file:
     config = json.load(data_file)
 
 with open('users.json') as data_file:
     users = json.load(data_file)
 
+# Setting up Watson API's
 conversation = ConversationV1(
   username=config['conversation']['username'],
   password=config['conversation']['password'],
   version='2017-02-03'
 )
 
-# Replace with the context obtained from the initial request
+# Variables
 global context
 context = {}
-
 workspace_id = 'fba7b3e5-6881-416c-9cf1-578fce2a9fee'
 
+''' Authenticates/logs-in user '''
+def authenticate(acc_no,pincode):
+    if(users[acc_no]['pincode'] == pincode):
+        return True
+    else:
+        return False
 
 
+'''
+Deals With Various responses and Updates Context Variables for directing conversation flow
+'''
+def dealWith(response):
+
+    if('verified' in response['context']):
+        
+        if(response['context']['verified'] == "1" and response['output']['nodes_visited'][0] == 'bank-balance'):
+            acc_no = response['context']['acc_no']
+            print(users[acc_no]['name'] + "'s account balance is " + users[acc_no]['balance'] + " " +users[acc_no]['currency'])
+
+    if(response['output']['nodes_visited'][0] == 'account-number'):
+        response['context']['acc_no'] = response['entities'][0]['value']
+        acc_no = response['context']['acc_no']
+        response['context']['pincode'] = users[acc_no]['pincode']
+
+
+    return response
+
+
+''' Helper functions '''
+def printJSON(printThis):
+    print(json.dumps(printThis, indent=2))
+
+''' Driver main function '''
 def converse(inputline):
     global context
-
-
     response = conversation.message(
       workspace_id=workspace_id,
       message_input={'text': inputline},
       context=context
     )
-
+    printJSON(response['output']['text'])
+    response = dealWith(response)
     context = response['context']
-    print(response['output'])
-
-    '''
-    if(response['output']['text'] != [] and response['entities'] != [] and response['entities'][0]['entity'] == 'account' and response['entities'][1]['entity'] == 'sys-number'):
-        acc_no = response['entities'][1]['value']
-        print("The balance of " + users[acc_no]['name'] + "is " +  users[acc_no]['balance'])
-    '''
 
 
+
+''' Command line interface'''
 for line in fileinput.input():
     converse(line.strip())
-
-
-
-
-
-print(json.dumps(response, indent=2))
